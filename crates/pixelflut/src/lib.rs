e50b00fc2b9cce85f56fd4c7ccc9ef96282e5dce
Philpax <me@philpax.me>
2022-06-04 19:02:32 +0200

    Add Pixelflut::read

--- crates/pixelflut/src/lib.rs
+++ crates/pixelflut/src/lib.rs
@@ -30,6 +30,24 @@ impl Pixelflut {
         Ok((iter.next().unwrap(), iter.next().unwrap()))
     }
 
+    pub fn read(&mut self, x: u32, y: u32) -> io::Result<(u8, u8, u8)> {
+        writeln!(self.write, "PX {} {}", x, y)?;
+
+        let mut line = String::new();
+        self.read.read_line(&mut line)?;
+
+        let colour_string = line
+            .split_ascii_whitespace()
+            .nth(3)
+            .expect("expected colour at end of PX");
+
+        Ok((
+            u8::from_str_radix(&colour_string[0..2], 16).unwrap(),
+            u8::from_str_radix(&colour_string[2..4], 16).unwrap(),
+            u8::from_str_radix(&colour_string[4..6], 16).unwrap(),
+        ))
+    }
+
     pub fn write(&mut self, x: u32, y: u32, color: (u8, u8, u8)) -> io::Result<()> {
         let hex = format!("{:02x}{:02x}{:02x}", color.0, color.1, color.2);
         writeln!(self.write, "PX {} {} {}", x, y, hex)
